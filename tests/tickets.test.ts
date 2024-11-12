6d4866f37e7e941ea3d7b17682a9625ac08d99ef
Matheus Henrique <matthpmoreira@gmail.com>
2024-11-12 07:54:10 -0300

    Test POST /tickets

--- tests/tickets.test.ts
+++ tests/tickets.test.ts
@@ -40,3 +40,64 @@ describe("GET /tickets/:id", () => {
     expect(status).toEqual(httpStatus.BAD_REQUEST);
   });
 });
+
+describe("POST /tickets", () => {
+  it("should create a ticket", async () => {
+    const { id: eventId } = await prisma.event.create({ data: EventFactory() });
+    const ticket = TicketFactory({ eventId });
+
+    const { status, body } = await api.post("/tickets").send(ticket);
+    const { id, ...response } = body;
+
+    expect(status).toEqual(httpStatus.CREATED);
+    expect(response).toEqual({ used: false, ...ticket });
+  });
+
+  it("should fail if a field is invalid", async () => {
+    const tickets = [
+      TicketFactory({ code: "" }),
+      TicketFactory({ owner: "" }),
+      TicketFactory({ eventId: 0 }),
+    ];
+
+    const status = await Promise.all(
+      tickets.map((ticket) =>
+        api
+          .post("/tickets")
+          .send(ticket)
+          .then((res) => res.status),
+      ),
+    );
+
+    expect(status).toEqual(
+      expect.arrayContaining([httpStatus.UNPROCESSABLE_ENTITY]),
+    );
+  });
+
+  it("should fail if already registered", async () => {
+    const { id: eventId } = await prisma.event.create({ data: EventFactory() });
+    const ticket = TicketFactory({ eventId });
+    await prisma.ticket.create({ data: ticket });
+
+    const { status } = await api.post("/tickets").send(ticket);
+
+    expect(status).toEqual(httpStatus.CONFLICT);
+  });
+
+  it("should fail if event expired", async () => {
+    const event = EventFactory({ date: faker.date.past() });
+    const { id: eventId } = await prisma.event.create({ data: event });
+    const ticket = TicketFactory({ eventId });
+
+    const { status } = await api.post("/tickets").send(ticket);
+
+    expect(status).toEqual(httpStatus.FORBIDDEN);
+  });
+
+  it("should fail if event not found", async () => {
+    const ticket = TicketFactory();
+    const { status } = await api.post("/tickets").send(ticket);
+
+    expect(status).toEqual(httpStatus.NOT_FOUND);
+  });
+});

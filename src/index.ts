38acfb09d4187953175d80b4d8956930b266d589
Matheus Henrique <matthpmoreira@gmail.com>
2024-11-12 07:47:06 -0300

    Split Express app and server initialization

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,10 @@ import eventsRouter from "./routers/events-router";
 
 dotenv.config();
 
-const app = express();
+export const app = express();
 app.use(json());
 
 app.get("/health", (req: Request, res: Response) => res.status(httpStatus.OK).send(`I'm okay!`));
 app.use(ticketsRouter);
 app.use(eventsRouter);
 app.use(errorHandlerMiddleware);
-
-const port = +process.env.PORT || 5000;
-app.listen(port, () => console.log("Server is up and running on port " + port));

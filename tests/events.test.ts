655727ed21f78a34bfea9063035a7b8d6e2b6000
Matheus Henrique <matthpmoreira@gmail.com>
2024-11-12 07:51:24 -0300

    Test GET /events/:id

--- tests/events.test.ts
+++ tests/events.test.ts
@@ -32,3 +32,29 @@ describe("GET /events", () => {
     );
   });
 });
+
+describe("GET /events/:id", () => {
+  it("should get a single event", async () => {
+    const { id } = await prisma.event.create({ data: EventFactory() });
+    const { status, body } = await api.get("/events/" + id);
+
+    expect(status).toEqual(httpStatus.OK);
+    expect(body).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        name: expect.any(String),
+        date: expect.any(String),
+      }),
+    );
+  });
+
+  it("should fail if ID is invalid", async () => {
+    const { status } = await api.get("/events/0");
+    expect(status).toEqual(httpStatus.BAD_REQUEST);
+  });
+
+  it("should fail if event does not exist", async () => {
+    const { status } = await api.get("/events/1");
+    expect(status).toEqual(httpStatus.NOT_FOUND);
+  });
+});
